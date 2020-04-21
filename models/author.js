e1ffec4e83cc3a7cab7bfa8e173a1b6c327523e0
LinGill21 <42749226+LinGill21@users.noreply.github.com>
2020-04-20 20:42:38 -0500

    fixed a few things and added more adds

--- models/author.js
+++ models/author.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-  firstName: { type: String, required: true},
-  LastName: { type: String, required: true},
+  first_name: { type: String, required: true},
+  family_name: { type: String, required: true},
+  date_of_birth: { type: Date }
 });
 
 module.exports = mongoose.model('Author', AuthorSchema);
